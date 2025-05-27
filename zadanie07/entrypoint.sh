5e7dbd3ea96e4d3fb51926dc6e8e8e63b2b99b71
noxikoxi <noxikoxi@gmail.com>
2025-05-27 15:27:49 +0200

    fix invalid filename

--- zadanie07/entrypoint.sh
+++ zadanie07/entrypoint.sh
@@ -2,8 +2,8 @@
 
 if [ ! -f ./db.sqlite ]; then
   echo "Pierwsze uruchomienie, wykonuję migracje"
-  ./zadanie07 migrate --yes
+  ./Run migrate --yes
 else
   echo "Baza już istnieje, pomijam migracje"
 fi
-./zadanie07 serve --env production --hostname 0.0.0.0 --port 8080
+./Run serve --env production --hostname 0.0.0.0 --port 8080
